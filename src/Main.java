6ebcb80c23a9b958d53e63a6b1a54f8b2acb4ac5
danitsoi <danitsoi@mail.ru>
2023-05-12 16:46:31 +0300

    accounts project, second commit

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,9 @@ import java.util.Arrays;
 
 public class Main {
 
-
-
     public static void main(String[] args) {
 
-
-
+        System.out.println("privet");
 //        int[] arr = {1, 2, 3, 4, 5, 6};
 //        System.out.println(Arrays.toString(reverseArray(arr)));
 
